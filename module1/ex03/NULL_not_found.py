7bf4fc202ff2239c85d7c6b5b0e09aaf90e95ea5
Hripsime <grigoryanhripsime18@gmail.com>
2025-11-15 17:00:06 +0400

    added

--- module1/ex03/NULL_not_found.py
+++ module1/ex03/NULL_not_found.py
@@ -1,26 +1,22 @@
+import math
+
 def NULL_not_found(object: any) -> int:
+    if object is None:
+        heading = "Nothing"
+    elif type(object) == float and math.isnan(object):
+        heading = "Cheese"
+    elif type(object) == int and object == 0:
+        heading = "Zero"
+    elif type(object) == str and object == "":
+        heading = "Empty"
+    elif type(object) == bool and object == False:
+        heading ="Fake"
+    else:
+        print("Type not Found")
+        return 1
+    
+    print(heading, ": ", object, " ", type(object))
+    return 0
 
 
 
-Nothing = None
-Garlic = float("NaN")
-Zero = 0
-Empty = ""
-Fake = False
-NULL_not_found(Nothing)
-NULL_not_found(Garlic)
-NULL_not_found(Zero)
-NULL_not_found(Empty)
-NULL_not_found(Fake)
-print(NULL_not_found("Brian"))
-
-
-# $>python tester.py | cat -e
-# Nothing: None <class 'NoneType'>$
-# Cheese: nan <class 'float'>$
-# Zero: 0 <class 'int'>$
-# Empty: <class 'str'>$
-# Fake: False <class 'bool'>$
-# Type not Found$
-# 1$
-# $>
