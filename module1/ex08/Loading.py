fdecda69d1b5c6461c9554c6aea8303e8f451fdd
Hripsime Grigoryan <hrigrigo@c2r3s5.42yerevan.am>
2026-01-21 20:59:09 +0400

    ex08 added

---
+++ module1/ex08/Loading.py
@@ -0,0 +1,24 @@
+from os import get_terminal_size 
+
+def ft_tqdm(lst: range) -> None:
+    """
+    This is an implementation of python original tqdm,
+    which simple prints out loading bar.
+    """
+    total_len = len(lst)
+    start = lst.start
+
+    for i, value in enumerate(lst, 1):
+        try: 
+            terminal_width = get_terminal_size().columns
+        except:
+            terminal_width = 80
+        percent = i / total_len
+        percent_str = f"{percent * 100 : 3.0f}%"
+        bar_width = max(10, terminal_width - len(percent_str) - 20)
+        filled_segment = int(bar_width * percent)
+        bar = "=" * filled_segment + " " * (bar_width - filled_segment)
+        print(f"\r{percent_str}|{bar}| {i}/{total_len}", end="", flush=True)
+        yield value
+
+    print()
