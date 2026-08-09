fc7837e22f3c95a3641ce274c3d55a1919070f66
hripsimegrigoryan <hrigoryanhripsime18@gmail.com>
2026-08-09 13:30:11 +0400

    revert

--- module1/ex09/README.md
+++
@@ -1,3 +0,0 @@
-# ft_package
-
-A sample package for counting values in a list.
--- module1/ex09/ft_package/count_in_list.py
+++
@@ -1,5 +0,0 @@
-def count_in_list(lst: list, word: str) -> int:
-    """
-    This method returns the number of arguments in in list.
-    """
-    return lst.count(word)
